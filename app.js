3ce1e2bc2c70c7fb81d1d87a46e619c1722818f7
Paul Rohorzka <paul@paulroho.com>
2015-10-04 00:54:47 +0200

    implemented parser

--- app.js
+++ app.js
@@ -8,8 +8,13 @@
 	function hookEventHandler() {
 		var btnStart = document.getElementById("start");
 		btnStart.addEventListener('click', function() {
-			var code = 'TODO';
-			var steps = parser.parse(code);
+			var code = 'xGehe nach rechts\r\nGehe nach rechts\r\nGehe nach rechts\r\nNimm den Käse\r\nGehe nach rechts\r\nIss den Käse';
+			try {
+				var steps = parser.parse(code);
+			}
+			catch (xcp) {
+				alert(xcp);
+			}
 			animator.run(steps);
 		});
 	}
--- parser.js
+++ parser.js
@@ -1,17 +1,39 @@
 'use strict';
 
 var parser = (function() {
+	function getStep(line) {
+		switch(line) {
+			case "Gehe nach rechts":
+				return moveRight();
+			case "Nimm den Käse":
+				return pickupTheCheese();
+			case "Iss den Käse":
+				return eatTheCheese();
+			default:
+				throw 'Die Anweisung "' + line + '" ist ungültig!';
+		}
+	}
+	
 	return {
 		parse: function(code) {
-			var steps = [moveRight('move w/o cheese #1'), 
-						 moveRight('move w/o cheese #2'), 
-						 moveRight('move w/o cheese #3'),
-						 // moveRight('move w/o cheese #4'),
-						 pickupTheCheese(),
-						 moveRight('move with cheese #1'),
-						 // moveRight('move with cheese #2'),
-						 // moveRight('move with cheese #3'),
-						 eatTheCheese()];
+			var steps = [];
+			
+			var lines = code.match(/[^\r\n]+/g);
+			console.log(lines);
+			for (var i=0; i<lines.length; i++) {
+				var line = lines[i];
+				var step = getStep(line);
+				steps.push(step);
+			}
+			// var steps = [moveRight('move w/o cheese #1'), 
+						 // moveRight('move w/o cheese #2'), 
+						 // moveRight('move w/o cheese #3'),
+						 // // moveRight('move w/o cheese #4'),
+						 // pickupTheCheese(),
+						 // moveRight('move with cheese #1'),
+						 // // moveRight('move with cheese #2'),
+						 // // moveRight('move with cheese #3'),
+						 // eatTheCheese()];
 			return steps;
 		}
 	};
