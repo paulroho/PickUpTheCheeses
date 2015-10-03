b81372eea7cc7ed3fb35b07cc5d87ff79bb1517c
Paul Rohorzka <paul@paulroho.com>
2015-10-03 23:00:57 +0200

    First step towards multiple steps

--- app.js
+++ app.js
@@ -4,12 +4,15 @@
 	var mice = document.getElementsByClassName("mouse");
 	var mouse = mice[0];
 	var mouseLeft = 100;
+	var maxMouseLeft = 400;
+	var currStep;
 	
 	// https://hacks.mozilla.org/2011/08/animating-with-javascript-from-setinterval-to-requestanimationframe/
 	function animLoop( render ) {
 		var running = true;
 		var lastFrame = undefined;
 		function loop( now ) {
+			console.log('loop ' + now);
 			if (typeof(lastFrame) === 'undefined') {
 				lastFrame = now;
 			}
@@ -23,10 +26,27 @@
 		requestAnimationFrame(loop);
 	}
 	
+	currStep = moveRight;
 	animLoop(function(delta) {
-		mouseLeft += 2 * delta / 16;
-		mouse.style.left = mouseLeft + 'px';
-		if (mouseLeft >= 400)
-			return false;
+		var proceedWithCurrentStep = currStep(delta);
+		if (proceedWithCurrentStep !== true) {
+			currStep = getNextStep();
+			return typeof(currStep) !== 'undefined';
+		}
+		return proceedWithCurrentStep;
 	});
+	
+	function getNextStep() {
+		return undefined;
+	}
+	
+	function moveRight(delta) {
+		mouseLeft += 3 * delta / 16;
+		var proceed = mouseLeft < maxMouseLeft;
+		if (!proceed) {
+			mouseLeft = maxMouseLeft;
+		}
+		mouse.style.left = mouseLeft + 'px';
+		return proceed;
+	}
 })();
